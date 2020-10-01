48733643ce54ed0e79f7c4c3a79585bd7324f058
AndreiPetrusMihai <68653908+AndreiPetrusMihai@users.noreply.github.com>
2020-10-01 16:45:24 +0300

    Updated routing for every shop category, modified the selectors, and created the new collections pages.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+import {Route} from 'react-router-dom';
 
-class ShopPage extends React.Component{
-	constructor(props){
-		super(props);
+import CollectionPage from '../collection/collection.component.jsx';
 
-		this.state={
-			collections: SHOP_DATA
-		}
-	}
+import CollectionOverview from '../../components/collection-overview/collection-overview.component.jsx';
 
-	render(){
-		const {collections} = this.state;
-		return (
-			<div className='shop-page'>
-				{
-					collections.map(({id,...otherCollectionProps}) => (
-						<CollectionPreview key={id} {...otherCollectionProps} />
-					))
-				}
-			</div>
-		)
-	}
+const ShopPage = ({match}) =>(
+	<div className='shop-page'>
+		<Route exact path={`${match.path}`} component={CollectionOverview}/>
+		<Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+	</div>
+)
 
-}
+
 
 export default ShopPage;
